f88fa94b414b7e340a43f7e5cbc9016ec5b1b26f
TFisher <tgfisher26@gmail.com>
2018-10-11 19:39:15 -0400

    Modules Installed and Package Set Up

---
+++ index.js
@@ -0,0 +1,6 @@
+var request = require("request");
+var fs = require("fs");
+var dotenv = require('dotenv').config();
+var Spotify = require('node-spotify-api');
+var moment = require('moment');
+moment().format();
